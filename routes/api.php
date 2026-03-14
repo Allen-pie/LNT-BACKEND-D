48c4e1870330abdd8ca9722965658efac6679e55
Pieter Allen <pieter.allen@binus.ac.id>
2026-03-14 21:15:30 +0700

    Add pertemuan 13

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -13,3 +15,24 @@ Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'delete']);
+
+
+Route::post('/test-mail', function(Request $request){
+   try {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'name' => 'required'
+        ]);
+
+        Mail::to($validated['email'])->send(new TestMail($validated['name']));
+
+        return response()->json([
+            'message' => 'email sucessfully sent'
+        ]);
+
+   }catch(Exception $error){
+        return response()->json([
+            'message' => $error->getMessage()
+        ], 500);
+   }
+});
